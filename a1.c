00c269663ea564678910aa256868aff943030bbe
LinuxMan <185729226@qq.com>
2019-10-14 14:47:59 +0800

    test c library

---
+++ a1.c
@@ -0,0 +1,13 @@
+#include "a.h"
+
+void randNum(int *arr){
+	int i;
+	srand((int)time(0));
+	for(i=0;i<100;i++){
+		arr[i] = rand()%100+1;
+		printf("%d \t", arr[i]);
+		if((i+1)%10 == 0){
+			printf("\n");
+		}
+	}
+}
